a0e552b1c05dce61184773f1b4a5f4b051c3b8f5
Jae McLain <jae.mclain@gmail.com>
2020-11-23 14:22:45 -0800

    updated code within files to reflect burger

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Plain');
+INSERT INTO cats (name) VALUES ('Cheese');
+INSERT INTO cats (name, sleepy) VALUES ('Vegan', true);
+INSERT INTO cats (name, sleepy) VALUES ('Vegan Cheese', true);
+INSERT INTO cats (name, sleepy) VALUES ('Double Bacon Cheese', true);
+INSERT INTO cats (name) VALUES ('Bacon');
